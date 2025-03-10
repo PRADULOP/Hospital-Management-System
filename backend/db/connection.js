f7835a402359ba1a205d8256101edc915142977e
cristeenayesudasan <cristeenayesudasan@gmail.com>
2025-03-10 14:11:49 +0530

    database created

--- backend/db/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.mongoDB_URL).then(() => {
-    console.log("Connection established to DB");
-}).catch(() => {
-    console.log("Not Connected");
-})
