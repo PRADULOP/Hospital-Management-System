7ff05a364c3e309cae115c233f51332e21542380
cristeenayesudasan <cristeenayesudasan@gmail.com>
2025-03-11 15:04:16 +0530

    created login feature for all users

---
+++ backend/controllers/userController.js
@@ -0,0 +1,103 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+import userModel from "../models/userModel.js";
+
+
+// API to register user
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password} = req.body;
+
+        // checking for all data to register user
+        if (!name || !email || !password) {
+            return res.json({ success: false, message: 'Missing Details' });
+        }
+
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" });
+        }
+
+        // validating strong password
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" });
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const userData = {
+            name,
+            email,
+            password: hashedPassword,
+            role: "user" 
+        };
+
+        const newUser = new userModel(userData);
+        const user = await newUser.save();
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
+
+        res.json({ success: true, token, role: user.role });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+// API to login user
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: "User does not exist" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            // Include user role in the JWT payload
+            const token = jwt.sign(
+                { id: user._id, role: user.role }, 
+                process.env.JWT_SECRET
+            );
+            
+            // Return token and role information
+            res.json({ 
+                success: true, 
+                token,
+                role: user.role
+            });
+        } else {
+            res.json({ success: false, message: "Invalid credentials" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+
+// API to get user profile data
+const getProfile = async (req, res) => {
+
+    try {
+        const { userId } = req.body
+        const userData = await userModel.findById(userId).select('-password')
+
+        res.json({ success: true, userData })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export {
+    loginUser,
+    registerUser,
+    getProfile
+};
